14bc8ef5f8399b1bc9bcef0fa83547e03012c402
Vivan <vivanvatsa@gmail.com>
2021-03-27 13:41:37 +0530

    added: python script for same init_plot

---
+++ init_plots/init_plot.py
@@ -0,0 +1,20 @@
+from matplotlib import pyplot as plt
+
+# Median Developer Salaries w.r.t Age
+x_coordinates = [25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35]
+y_coordinates = [
+    38496,
+    42000,
+    46752,
+    49320,
+    53200,
+    56000,
+    62316,
+    64928,
+    67317,
+    68748,
+    73752,
+]
+
+plt.plot(x_coordinates, y_coordinates)
+plt.show()
