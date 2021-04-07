201f56c61ac08432212fa86f72bf6cdd8141a34b
Vivan <vivanvatsa@gmail.com>
2021-04-07 22:01:59 +0530

    added: pie chart internals

--- pie-charts/pie_chart.py
+++ pie-charts/pie_chart.py
@@ -4,13 +4,12 @@ plt.style.use('fivethirtyeight')
 # this will be a list
 slices = [60, 40]
 # slices = [120, 80]
-labels = ['sixty', 'forty']
+labels = ['sixty', 'forty', 'extra1', 'extra2']
+colors = ['blue', 'red', 'yellow', 'green']
 
 
-
-
-plt.pie(slices, labels = labels)
-
+plt.pie(slices, labels = labels, colors=colors, wedgeprops={'edgecolor': 'black'})
+# wedgeprops: in the documentation
 # add labels to the list to be displayed inside the slides
 
 
@@ -18,3 +17,9 @@ plt.pie(slices, labels = labels)
 plt.title('awesome pie chart') # head of the plot
 plt.tight_layout()
 plt.show()
+
+# Colors:
+# Blue = #008fd5
+# Red = #fc4f30
+# Yellow = #e5ae37
+# Green = #6d904f
