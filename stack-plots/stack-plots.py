60a12772778c573518ca54af69e4d620b5ca44b6
Vivan <vivanvatsa@gmail.com>
2021-04-08 21:46:17 +0530

    added: stackplot degenracy

--- stack-plots/stack-plots.py
+++ stack-plots/stack-plots.py
@@ -1,17 +1,22 @@
 # stack-plot = can be plotted when
 # if there is a total and want of breakdown of a total is required
+# PTR = always check stack plot maintains constant total
 from matplotlib import pyplot as plt
-
 plt.style.use("fivethirtyeight")
-
 
 minutes = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-player1 = [1, 2, 3, 3, 4, 4, 4, 4, 5]
-player2 = [1, 1, 1, 1, 2, 2, 2, 3, 4]
-player3 = [1, 1, 1, 2, 2, 2, 3, 3, 3]
+player1 = [8, 6, 5, 5, 4, 2, 1, 1, 0]
+player2 = [0, 1, 2, 2, 2, 4, 4, 4, 4]
+player3 = [0, 1, 1, 1, 2, 2, 3, 3, 4]
 
-plt.pie([1, 1, 1], labels=["Player 1", "Player2", "Player3"])
+labels = ['player1', 'player2', 'player3']
+colors = ['#6d904f', '#fc4f30', '#008fd5']
+
+plt.stackplot(minutes, player1, player2, player3, labels=labels, colors=colors)
+
+plt.legend(loc = 'lower left')
+# plt.legend() - default legend = it already find a suitable place, but for manual placement
 
 plt.title("viv Stack Plot")
 plt.tight_layout()
