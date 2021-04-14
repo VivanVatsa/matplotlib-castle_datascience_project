b66adf7cb12f4fd43cc998df4021bbc034ac73f8
Vivan <vivanvatsa@gmail.com>
2021-04-14 22:10:46 +0530

    added: time series coe

--- live-plotting/live-plotting.py
+++ live-plotting/live-plotting.py
@@ -2,6 +2,7 @@ import random
 from itertools import count
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 
 plt.style.use('fivethirtyeight')
 
@@ -14,6 +15,9 @@ def animate(i):
     x_vals.append(next(index))
     y_vals.append(random.randint(0, 5))
 
+    plt.plot(x_vals, y_vals)
+
+ani = FuncAnimation(plt.gcf(), animate, interval=1000)
 
 plt.tight_layout()
 plt.show()
