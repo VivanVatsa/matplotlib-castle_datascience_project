88f73939f69dc43e2d681558afd4f91086bea1c6
Vivan <vivanvatsa@gmail.com>
2021-04-14 22:37:02 +0530

    added: live-data generation code

--- live-plotting/live-plotting.py
+++ live-plotting/live-plotting.py
@@ -7,7 +7,7 @@ from matplotlib.animation import FuncAnimation
 plt.style.use('fivethirtyeight')
 
 x_vals = []
-y_vals = []
+y_vals = [] 
 
 index = count()
 
@@ -15,9 +15,13 @@ def animate(i):
     x_vals.append(next(index))
     y_vals.append(random.randint(0, 5))
 
+    plt.cla()
     plt.plot(x_vals, y_vals)
-
-ani = FuncAnimation(plt.gcf(), animate, interval=1000)
+    # output => random lines with random colors!!
+    # why? => there are multiple lines are just stacking up & they are just being covered up and no info is seen
+    # 
+ 
+ani = FuncAnimation(plt.gcf(), animate, interval=1000) 
 
 plt.tight_layout()
 plt.show()
