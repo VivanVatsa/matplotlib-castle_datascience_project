8ea276a301db69b8ae2db08bc66ae145d9ab6c61
Vivan <vivanvatsa@gmail.com>
2021-04-14 10:55:36 +0530

    done:time series plot (+csv)

--- time-series-plots/time-series-plot.py
+++ time-series-plots/time-series-plot.py
@@ -5,26 +5,43 @@ from matplotlib import dates as mpl_dates
 
 plt.style.use('seaborn')
 
-dates = [
-    datetime(2019, 5, 24),
-    datetime(2019, 5, 25),
-    datetime(2019, 5, 26),
-    datetime(2019, 5, 27),
-    datetime(2019, 5, 28),
-    datetime(2019, 5, 29),
-    datetime(2019, 5, 30)
-]
+data = pd.read_csv('data.csv')
 
-y = [0, 1, 3, 4, 6, 5, 7]
+data['Date'] = pd.to_datetime(data['Date'])
+data.sort_values('Date', inplace=True)
 
-# data = pd.read_csv('data.csv')
-# price_date = data['Date']
-# price_close = data['Close']
+price_date = data['Date']
+price_close = data['Close']
 
-# plt.title('Bitcoin Prices')
-# plt.xlabel('Date')
-# plt.ylabel('Closing Price')
+# plt.plot_date(dates, y, linestyle='solid')
+plt.plot_date(price_date, price_close, linestyle='solid')
 
+# gcf = get current figure
+plt.gcf().autofmt_xdate()
+
+plt.title('Bitcoin Prices')
+plt.xlabel('Date')
+plt.ylabel('Closing Price')
+
+# adds padding to the plot
 plt.tight_layout()
 
 plt.show()
+
+
+# dates = [
+#     datetime(2019, 5, 24),
+#     datetime(2019, 5, 25),
+#     datetime(2019, 5, 26),
+#     datetime(2019, 5, 27),
+#     datetime(2019, 5, 28),
+#     datetime(2019, 5, 29),
+#     datetime(2019, 5, 30)
+# ]
+
+# y = [0, 1, 3, 4, 6, 5, 7]
+
+
+# date_format = mpl_dates.DateFormatter('%b, %d, %Y')
+# gca = get current axis	
+# plt.gca().xaxis.set_major_formatter(date_format)
